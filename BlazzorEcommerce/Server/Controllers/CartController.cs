d5c13b5decca9dfc88aeae4442f5fe3cbad5efb3
Domas Jonaitis <domasjon16@gmail.com>
2022-03-21 13:54:41 +0200

    Added pagination and cart

---
+++ BlazzorEcommerce/Server/Controllers/CartController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazzorEcommerce.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CartController : ControllerBase
+    {
+        private readonly ICartService cartService;
+
+        public CartController(ICartService cartService)
+        {
+            this.cartService = cartService;
+        }
+
+        [HttpPost("products")]
+        public async Task<ActionResult<ServiceResponse<List<CartProductResponse>>>> GetCartProducts(List<CartItem> cartItems)
+        {
+            var result = await this.cartService.GetCartProducts(cartItems);
+            return Ok(result);
+        }
+    }
+}
